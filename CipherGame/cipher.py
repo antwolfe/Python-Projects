348d473ecb37d67d217efddd7d6b1fee4b054e7a
antwolfe <muddywolfe77@gmail.com>
2021-10-09 00:42:56 -0500

    Cipher folder neede

---
+++ CipherGame/cipher.py
@@ -0,0 +1,42 @@
+# from cipherart import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def ceasar(start_text, shift_amount, cip_direction):
+    start_text = ""
+    for letter in text:
+        # if text not in alphabet:
+        # TODO-3: What happens if the user enters a number/symbol/space?
+        # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+        # e.g. start_text = "meet me at 3"
+        # end_text = "•••• •• •• 3"
+        position = alphabet.index(letter)
+        if direction == "encode":
+            new_position = position + shift_amount
+        elif direction == "decode":
+            new_position = position - shift_amount
+        new_letter = alphabet[new_position]
+        start_text += new_letter
+    print(f"The {cip_direction}d message is {start_text}")
+
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+#Hint: Think about how you can use the modulus (%).
+shift = shift % 26
+
+
+ceasar(text, shift, direction)
