7c60109ed23f4840d9afab4c469a43b99ba9edb4
antwolfe <43706281+antwolfe@users.noreply.github.com>
2021-10-09 00:44:39 -0500

    Update cipher.py

--- CipherGame/cipher.py
+++ CipherGame/cipher.py
@@ -1,4 +1,4 @@
-# from cipherart import logo
+from cipherart import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
